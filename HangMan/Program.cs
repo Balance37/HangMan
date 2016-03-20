8edb207fcd6d4cafdab2dca5d7bf2fcb0207de67
June Wang <June Wang>
2016-03-20 15:52:05 -0400

    first commit

---
+++ HangMan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User A please enter a word: ");
+            string word = Console.ReadLine();
+            char[] characters = word.ToCharArray();
+            int length = characters.Length;
+            int charLeft = length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("User B please guess the word and enter a character, you have {0} time(s) left:", charLeft-1);
+                char guess = Console.ReadKey().KeyChar;
+                if(guess)
+                charLeft--;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
